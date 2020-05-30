ee01e64c0afc7d94f4d449283452feb7ad8439e0
Gurkirat Singh <44253465+gurkiratsinghofficial@users.noreply.github.com>
2020-05-30 20:56:46 +0530

    Update Ctable.js

--- src/components/Ctable.js
+++ src/components/Ctable.js
@@ -13,14 +13,14 @@ class Ctable extends Component {
         //console.log("Props",this.props);
         console.log("Hello",state,districts.data[state]);
         return (
-            <Table striped bordered hover size="sm" responsive style={{ margin: "0px",padding:"0px" }}>
+            <Table striped  hover size="sm" responsive style={{ margin: "0px",padding:"0px" }}>
         <small>
             <thead>
-              <tr>
-              <th>#</th>
+              <tr style={{background:"light"}}>
+              <th>District</th>
                 <th>Active</th>
-                <th>Cases</th>
-                
+                <th>Confirmed</th>
+                <th>Recovered</th>
                 <th>Deaths</th>
               </tr>
               {
@@ -35,7 +35,7 @@ class Ctable extends Component {
                              districts.data[state].districtData[value].delta.confirmed !==0 ?  <Badge pill variant="warning"><small><i className="fas fa-arrow-up"/></small>{ districts.data[state].districtData[value].delta.confirmed}</Badge>:<></>
                         }
                             {districts.data[state].districtData[value].confirmed}</td>
-                        
+                        <td>{districts.data[state].districtData[value].recovered}</td>
                         <td>{districts.data[state].districtData[value].deceased}</td>
                               </tr>
                     )
