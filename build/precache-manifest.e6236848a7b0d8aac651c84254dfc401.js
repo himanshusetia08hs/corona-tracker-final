f3b7b637bc8f67288934d081c66409ad2bc8dc07
Gaurav Setia <gsetia@docmation.com>
2021-08-09 01:18:11 +0530

    final-deployment

---
+++ build/precache-manifest.e6236848a7b0d8aac651c84254dfc401.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e784d1d601d6fe9af4098044e862673e",
+    "url": "/corona-tracker-final/index.html"
+  },
+  {
+    "revision": "ac1ef9f5068d12448c50",
+    "url": "/corona-tracker-final/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "5fb7a2736093dc9b3d36",
+    "url": "/corona-tracker-final/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "ac1ef9f5068d12448c50",
+    "url": "/corona-tracker-final/static/js/2.02027005.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/corona-tracker-final/static/js/2.02027005.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fb7a2736093dc9b3d36",
+    "url": "/corona-tracker-final/static/js/main.e324d602.chunk.js"
+  },
+  {
+    "revision": "2b155882320d486bd86a",
+    "url": "/corona-tracker-final/static/js/runtime-main.e3cea891.js"
+  }
+]);
